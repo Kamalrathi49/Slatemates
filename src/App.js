a063cbaf67832d22375bbdf1beff5f514d3fa9ab
Kamal Rathi <kamalrathi049@gmail.com>
2021-06-13 14:37:27 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,47 +1,78 @@
 import React from "react";
+import "./App.css";
 
-import "./App.scss";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container } from "@material-ui/core/";
+import Grid from "@material-ui/core/Grid";
+import "@material-ui/core";
+import "@fontsource/roboto";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    ...theme.typography.button,
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(1.5),
+    flexGrow: 1,
+    backgroundColor: "#ffff8d",
+    color: "#311b92",
+    textAlign: "center",
+  },
+}));
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div className="app">
-      <div className="header">
-        <h1 className="title">Very very Carefully Chosen Tutors</h1>
-      </div>{" "}
-      <br />
-      <br />
-      <div className="footer">
-        <p className="footer-heading">
-          Cambridge & IB Mathematics Subject choice
-        </p>
-        <div className="contain">
-          <div className="col">
-            <h1>IGCSE - Mathematics</h1>
-            <ul>
-              <li>Mathematics Core/Extended </li>
-              <li>International Mathematics </li>
-              <li>Additional Mathematics</li>
-            </ul>
-          </div>
-          <div className="col">
-            <h1>AS-A level</h1>
-            <ul>
-              <li>Mathematics 9709</li>
-              <li>Mathematics Further 9231</li>
-            </ul>
-          </div>
-          <div className="col">
-            <h1>IBDP</h1>
-            <ul>
-              <li>Mathematics AA SL</li>
-              <li>Mathematics AA HL</li>
-              <li>Mathematics AI SL</li>
-              <li>Mathematics AI HL</li>
-            </ul>
-          </div>
+    <Container maxWidth="lg">
+      <Grid
+        container
+        direction="column"
+        justify="flex-start"
+        alignItems="center"
+      >
+        <div>
+          <h1 className={classes.root}>Very very Carefully Chosen Tutors</h1>
+        </div>{" "}
+        <br />
+        <br />
+        <div style={{ backgroundColor: "green", width: "60%" }}>
+          <p className={classes.root}>
+            Cambridge & IB Mathematics Subject choice
+          </p>
+
+          <Grid className="table"
+            container
+            direction="row"
+            justifyContent="flex-start"
+            style={{ fontSize: "13px", justifyContent: "space-evenly" }}
+          >
+            <div>
+              <h1>IGCSE - Mathematics</h1>
+              <ul className="list">
+                <li>Mathematics Core/Extended </li>
+                <li>International Mathematics </li>
+                <li>Additional Mathematics</li>
+              </ul>
+            </div>
+            <div>
+              <h1>AS-A level</h1>
+              <ul className="list">
+                <li>Mathematics 9709</li>
+                <li>Mathematics Further 9231</li>
+              </ul>
+            </div>
+            <div>
+              <h1>IBDP</h1>
+              <ul className="list">
+                <li>Mathematics AA SL</li>
+                <li>Mathematics AA HL</li>
+                <li>Mathematics AI SL</li>
+                <li>Mathematics AI HL</li>
+              </ul>
+            </div>
+          </Grid>
         </div>
-      </div>
-    </div>
+      </Grid>
+    </Container>
   );
 };
 
